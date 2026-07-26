614dcee48e2f96fc055e41d5cb258d19d17069c8
Magnus Lyckå <magnus.lycka@gmail.com>
2026-07-26 17:46:36 +0200

    vulnerabilities4

--- public/eslint.config.mjs
+++ public/eslint.config.mjs
@@ -1,19 +1,25 @@
 export default [
   {
+    ignores: ["js/bundle.js", "js/bundle-stats.js", "js/lib/**"],
+  },
+  {
     files: ["js/**/*.js"],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: "module",
       globals: {
         Blob: "readonly",
+        clearTimeout: "readonly",
         console: "readonly",
         document: "readonly",
         fetch: "readonly",
         FileReader: "readonly",
         Image: "readonly",
         L: "readonly",
+        localStorage: "readonly",
         m: "readonly",
         performance: "readonly",
+        setInterval: "readonly",
         setTimeout: "readonly",
         THREE: "readonly",
         URL: "readonly",
@@ -23,7 +29,7 @@ export default [
     },
     rules: {
       "no-undef": "error",
-      "no-unused-vars": ["error", { args: "none" }],
+      "no-unused-vars": ["error", { args: "none", caughtErrors: "none" }],
     },
   },
 ];
